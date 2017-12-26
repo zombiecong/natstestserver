764e7b2c6aa752d2393adaa7c3f086bf808dca95
caelums <ccaelums@gmail.com>
2017-12-26 16:29:27 +0800

    update

--- main.go
+++ main.go
@@ -1,1 +1,40 @@
-package natstestserver
+package main
+
+import (
+	"github.com/nats-io/go-nats"
+	"time"
+	"fmt"
+	"os"
+)
+
+func t()  {
+	now := time.Now()
+	year, mon, day := now.UTC().Date()
+	hour, min, sec := now.UTC().Clock()
+	zone, _ := now.UTC().Zone()
+	fmt.Printf("UTC 时间是 %d-%d-%d %02d:%02d:%02d %s\n",
+		year, mon, day, hour, min, sec, zone)
+}
+
+
+func main()  {
+
+
+	argsWithoutProg := os.Args[1:]
+
+	fmt.Println(argsWithoutProg)
+
+
+	nc, _ := nats.Connect(argsWithoutProg[0])
+
+	nc.Subscribe("send", func(m *nats.Msg){
+		fmt.Printf("Received a message: %s\n", string(m.Data))
+		t();
+		nc.Publish("receive",m.Data)
+	})
+	// Simple Publisher
+
+	for{
+		time.Sleep(time.Second)
+	}
+}
